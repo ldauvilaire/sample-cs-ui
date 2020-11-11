e7a72ac9a2d5448a504a60e52a11ddb99f9d9e65
Laurent Dauvilaire <ladauvilaire-ext@airfrance.fr>
2020-11-11 17:34:51 +0100

    Migration to Angular 9 - Step 1

--- src/app/core/router-state.ts
+++ src/app/core/router-state.ts
@@ -1,11 +1,13 @@
 import { RouterStateSerializer } from '@ngrx/router-store';
 import { RouterStateSnapshot, Params } from '@angular/router';
+import { Injectable } from "@angular/core";
 
 export interface RouterState {
   url: string;
   queryParams: Params;
 }
 
+@Injectable()
 export class CustomRouterStateSerializer implements RouterStateSerializer<RouterState> {
   serialize(routerState: RouterStateSnapshot): RouterState {
     const url = routerState.url;
